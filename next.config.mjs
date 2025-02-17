8936f47913f892722f1db096d9abd7eab1f7ab25
enakamizo <166042638+enakamizo@users.noreply.github.com>
2025-02-17 21:49:24 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-const express = require("express");
-const app = express();
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+};
 
-const port = process.env.PORT || 3000;  // Azure の環境変数 PORT を使用
-
-app.get("/", (req, res) => {
-  res.send("Hello, Azure!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export default nextConfig;
 
